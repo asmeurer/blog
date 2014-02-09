85d25c791a328ea2f1f79ad6bdfefe0335677072
Aaron Meurer <asmeurer@gmail.com>
2014-02-09 17:19:31 -0600

    Separate out the metadata in posts

--- conf.py
+++ conf.py
@@ -169,7 +169,7 @@ COMPILERS = {
 
 # Create by default posts in one file format?
 # Set to False for two-file posts, with separate metadata.
-# ONE_FILE_POSTS = True
+ONE_FILE_POSTS = False
 
 # If this is set to True, then posts that are not translated to a language
 # LANG will not be visible at all in the pages in that language.
--- posts/moving-to-github-pages-with-nikola.md
+++ posts/moving-to-github-pages-with-nikola.md
@@ -1,11 +1,1 @@
-<!-- 
-.. title: Moving to GitHub pages with Nikola
-.. slug: moving-to-github-pages-with-nikola
-.. date: 2014/02/09 17:17:42
-.. tags: 
-.. link: 
-.. description: 
-.. type: text
--->
-
 Write your post here.
