b3647a973c79bd4d1ae7cc219d6d006773429fd4
Aaron Meurer <asmeurer@gmail.com>
2014-04-04 20:13:50 -0500

    Don't use the sharethis button

--- conf.py
+++ conf.py
@@ -525,9 +525,14 @@ SOCIAL_BUTTONS_CODE = ("""<div style="text-align:center">
 """
 
 #<span class='st_linkedin_large' displayText='LinkedIn'></span>
+
 + """
 <span class='st_email_large' displayText='Email'></span>
-<span class='st_sharethis_large' displayText='ShareThis'></span>
+"""
+
+# <span class='st_sharethis_large' displayText='ShareThis'></span>
+
++ """
 </div>
 """)
 
