d44a11205c4040d69bdca5246d11068bf9b296be
Aaron Meurer <asmeurer@gmail.com>
2015-12-03 12:48:02 -0600

    Only show three posts on the front page

--- conf.py
+++ conf.py
@@ -551,7 +551,7 @@ SOCIAL_BUTTONS_CODE = ("""<div style="text-align:center">
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+INDEX_DISPLAY_POST_COUNT = 3
 
 # RSS_LINK is a HTML fragment to link the RSS or Atom feeds. If set to None,
 # the base.tmpl will use the feed Nikola generates. However, you may want to
